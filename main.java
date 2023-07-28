e646d8f58e1a89d7b058cb05a57dcf8489dabfda
NashiKao <54812643+F0rg3tM3@users.noreply.github.com>
2023-07-28 10:20:34 +0800

    Update main.java

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		bool status = True;
+		Bool status = True;
 		while (status) {
 			System.out.println("Test");
 		}
